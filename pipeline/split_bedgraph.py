31fa5e3a23887dbc7b66ea5519dea1c071bed02c
Rachel Ancar Class <ancarr@amc-bodhi.cluster.pvt>
2018-10-23 14:29:08 -0600

    Yay it works, new split logic

--- pipeline/split_bedgraph.py
+++ pipeline/split_bedgraph.py
@@ -3,21 +3,29 @@
 ''' split a bedgraph by chromosome '''
 
 import sys
+import os
 
 chrom_names = ["mhv" , "tRNA", "r18s" , "r28s" , "r5.8s" , "r5s" , "U6", "mRNA"]
+name = os.path.basename(sys.argv[1]).split(".")[0]
+out_dir = str(sys.argv[2]) 
+#sample = str(sys.argv[2]).split(".")[0]
 
 # set up the output files for writing
 outputs = {}
-for name in chrom_names:
-    outputs[name] = file("%s.bg" % name, "w")
+for chrom in chrom_names:
+    outpath = os.path.join(out_dir, chrom, name + "." + chrom + ".bg") 
+    outputs[chrom] = open(outpath, "w")
 
-for line in file(sys.argv[1]):
+for line in open(sys.argv[1]):
 
-    fields = line.split("\t")
-    chrom = fields[0].split("|")[0].strip(">")
+    chrom, start, end, count, normalized_count = line.split("\t")
+    #print(fields)
+    file_chrom = chrom.split("|")[0]
+    #chrom, start, end, count, normalized_count = fields[0:5]
     # look up the output file by chrom name
-    output = outputs[chrom]
-
-    print(chrom, start, end,count, normalized_count,
-          sep = "\t", file=output)
+    output = outputs[file_chrom]
+    
+    output.write(chrom + "\t" +  start + "\t" +  end + "\t" + count + "\t" + normalized_count)
+    #print(chrom, start, end,count, normalized_count,
+          #sep = "\t", file=output)
 
