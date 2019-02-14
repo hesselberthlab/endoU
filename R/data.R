6ba6f04dee620ca2c23d43fc39cad073007b2edb
Rachel Ancar <rachel.ancar@ucdenver.edu>
2019-02-14 10:07:05 -0700

    mhv_freq data

--- R/data.R
+++ R/data.R
@@ -1,2 +1,5 @@
 #' Viral coverage table
 "viral_cov_tbl"
+
+#' MHV background dinucleotide frequencies table
+"mhv_freq"
---
+++ data-raw/mhv_frequency.R
@@ -0,0 +1,18 @@
+library(endoU)
+library(tidyverse)
+library(fs)
+library(usethis)
+
+
+basedir <- "~/Dropbox (Hesselberth Lab)/Rachel_data/EndoU_project"
+dinuc <- c("AA","GA", "CA", "UA","AC", "CC", "GC", "UC", "AG", "CG", "GG", "UG", "AU", "CU", "GU", "UU")
+df_dinuc <- data_frame(dinuc)
+
+background_dinuc_counts <- fs::path_join(c(basedir, "mhv_fasta_dinuc_counts.txt"))
+
+mhv_freq <- readr::read_tsv(background_dinuc_counts) %>%
+  dplyr::mutate(frequency = count/sum(count))
+
+usethis::use_data(mhv_freq, compress = "xz", overwrite = TRUE)
+
+
