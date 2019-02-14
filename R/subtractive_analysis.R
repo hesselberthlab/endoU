22729644f56210e6112df008081eb4971281ea4d
Rachel Ancar <rachel.ancar@ucdenver.edu>
2019-02-14 11:32:42 -0700

    working on subtractive analysis

--- R/subtractive_analysis.R
+++ R/subtractive_analysis.R
@@ -16,13 +16,3 @@ RNaseL_sub <- function(x) {
 }
 
 
-
-
-#spread(cell, norm_count)
-  subtractive_neg[is.na(subtractive_neg)] <- 0
-
-subtractive_neg <- subtractive_neg %>%
-  mutate(B6_RNaseLdep = B6 - RNaseL) %>%
-  mutate(IFNAR_RNaseLdep = IFNAR - RNaseL) %>%
-  dplyr::select(start:end, virus:time, B6_RNaseLdep:IFNAR_RNaseLdep) %>%
-  gather(cell, normalized_count, B6_RNaseLdep:IFNAR_RNaseLdep)
