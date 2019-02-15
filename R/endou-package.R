d07f91bfbebba20b3dbd73c5f319754b10937799
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-02-15 08:48:26 -0700

    create package file for imports

---
+++ R/endou-package.R
@@ -0,0 +1,23 @@
+#' endoU: RNA-seq analysis of endoU cleavage sites
+#'
+#' @author Rachel Ancar <rachel.ancar@@ucdenver.edu>
+#' @author Jay Hesselberth <jay.hesselberth@@gmail.com>
+#'
+#' @docType package
+#' @name endoU
+#'
+#' @import readr
+#' @import dplyr
+#' @import tibble
+#' @import purrr
+#' @import ggplot2
+#'
+#' @importFrom tidyr gather nest crossing
+#'
+#' @importFrom glue glue
+#' @importFrom cowplot theme_cowplot plot_grid
+#' @importFrom rlang parse_quosure is_character is_logical quo_text
+#' @importFrom rlang list2 .data :=
+#' @importFrom magrittr %>%
+#'
+"_PACKAGE"
