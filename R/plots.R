76afc32d15ef502d2f580e4d9a55e0cafaefee36
Rachel Ancar <rachel.ancar@ucdenver.edu>
2019-02-22 00:53:25 -0700

    added some other pre-sets

--- R/plots.R
+++ R/plots.R
@@ -9,11 +9,12 @@
 #' @examples
 #' plot_coverage(
 #'   viral_cov_small,
-#'   start, norm_count,
+#'   pos, norm_count,
 #'   cell = "B6", virus = "ns2"
 #' )
 #'
 #' @export
+
 plot_coverage <- function(data, x, y, cell = NULL, virus = NULL) {
   x <- enquo(x)
   y <- enquo(y)
@@ -26,16 +27,18 @@ plot_coverage <- function(data, x, y, cell = NULL, virus = NULL) {
     data <- filter(data, virus == !!virus)
   }
 
-  ggplot(data, aes(x = !!x, y = !!y, fill = time, width = 25)) +
+  data <- data %>% mutate(time = fct_relevel(time, "9", "12"))
+
+  ggplot(data, aes(x = (!!x/1000), y = !!y, fill = time, width = 0.025)) +
     geom_bar(stat = "identity", position = "identity") +
     scale_fill_brewer(palette = "Set1") +
     theme_cowplot() +
+    ggtitle(paste(cell, virus, sep="_")) +
+    scale_y_continuous(limits = c(0.0, 0.2)) +
+    scale_x_continuous(breaks=seq(0, 30, 5)) +
     labs(
       x = "Position (kb)",
       y = "Normalized counts"
     )
 
 }
-
-
-
