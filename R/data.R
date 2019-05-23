318329f3d09fcc0f3d2adf6078e743e000fc2156
Rachel Ancar <rachel.ancar@ucdenver.edu>
2019-05-23 12:16:37 -0600

    Added data objects to data.R

--- R/data.R
+++ R/data.R
@@ -7,6 +7,24 @@
 #' Viral coverage table - without pseudo-counts for dinuc analysis
 "viral_cov_freq"
 
+#' Viral coverage table - without pseudo-counts for dinuc analysis -1 +1 posisitions
+"viral_cov_freq1"
+
+#' Viral coverage table - without pseudo-counts for dinuc analysis +1 +2 posisitions
+"viral_cov_freq2"
+
+#' Viral coverage table - without pseudo-counts for dinuc analysis +2 +3 posisitions
+"viral_cov_freq3"
+
+#' Viral coverage table -1 +1 posisitions
+"viral_cov_tbl1"
+
+#' Viral coverage table +1 +2 posisitions
+"viral_cov_tbl2"
+
+#' Viral coverage table +2 +3 posisitions
+"viral_cov_tbl3"
+
 #' MHV background dinucleotide frequencies table
 "mhv_freq"
 
@@ -21,3 +39,15 @@
 
 #' 5.8S coverage table
 "r5.8S_cov_tbl"
+
+#' all reads sizes table
+"all_reads"
+
+#' antigenomic viral cov table
+"antiviral_cov_tbl"
+
+#' mRNA cov table
+"mRNA_cov_tbl"
+
+#' U6 cov table
+"U6_cov_tbl"
