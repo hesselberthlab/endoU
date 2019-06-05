d7d5fe17bc26419172aa9869831e9ea28939b61b
Rachel Ancar <rachel.ancar@ucdenver.edu>
2019-06-05 16:01:56 -0600

    updated rRNA cov tables with new libary sizes method

--- data-raw/28_cov.R
+++ data-raw/28_cov.R
@@ -9,8 +9,17 @@ r28S_tabs <- fs::path_join(c(basedir, "28S/exp1/new"))
 norm_tabs <- fs::path_join(c(basedir, "all_RNA_bg/exp_1"))
 dinuc_counts <- fs::path_join(c(basedir, "dinuc_byposition/r28s.dinuc.txt"))
 
+filenames <- list_names(norm_tabs)
+
+library_sizes <- coverage_table(filenames) %>%
+  dplyr::select(count, cell, virus, time) %>%
+  dplyr::group_by(cell, virus, time) %>%
+  dplyr::mutate(total_umi_reads = sum(count)) %>%
+  dplyr::select(-count) %>%
+  unique()
+
 r28S_cov_tbl <- coverage_table(list_names(r28S_tabs))
 r28S_cov_tbl <- complete_table(r28S_cov_tbl, dinuc_counts)
-r28S_cov_tbl <- normalize_table(norm_tabs, r28S_cov_tbl)
+r28S_cov_tbl <- normalize_table(library_sizes, r28S_cov_tbl)
 
 usethis::use_data(r28S_cov_tbl, compress = "xz", overwrite = TRUE)
--- data-raw/5S_cov.R
+++ data-raw/5S_cov.R
@@ -9,8 +9,17 @@ r5S_tabs <- fs::path_join(c(basedir, "5S/new"))
 norm_tabs <- fs::path_join(c(basedir, "all_RNA_bg/exp_1"))
 dinuc_counts <- fs::path_join(c(basedir, "dinuc_byposition/r5s.dinuc.txt"))
 
+filenames <- list_names(norm_tabs)
+
+library_sizes <- coverage_table(filenames) %>%
+  dplyr::select(count, cell, virus, time) %>%
+  dplyr::group_by(cell, virus, time) %>%
+  dplyr::mutate(total_umi_reads = sum(count)) %>%
+  dplyr::select(-count) %>%
+  unique()
+
 r5S_cov_tbl <- coverage_table(list_names(r5S_tabs))
 r5S_cov_tbl <- complete_table(r5S_cov_tbl, dinuc_counts)
-r5S_cov_tbl <- normalize_table(norm_tabs, r5S_cov_tbl)
+r5S_cov_tbl <- normalize_table(library_sizes, r5S_cov_tbl)
 
 usethis::use_data(r5S_cov_tbl, compress = "xz", overwrite = TRUE)
